a7748a63b749025874de577ebdd4e8618b3330ae
Jamil Velji <jamil@jamilvelji.com>
2014-08-14 09:44:43 -0400

    Commented both files

--- step1.rb
+++ step1.rb
@@ -1,6 +1,7 @@
 # Make the following array [1, 12, 144, 1728, 20736, 248832, 2985984, 35831808]
 # without just inputting the numbers to the array
 
+# Solution 1
 a = []
 b = 1
 
@@ -12,6 +13,7 @@ end
 puts "\nTimes do method:\n"
 puts "#{a}\n\n"
 
+# Solution 2
 array_1 = [0, 1, 2, 3, 4, 5, 6, 7]
 
 array_2 = array_1.map {|x| 12 ** x}
@@ -19,6 +21,7 @@ array_2 = array_1.map {|x| 12 ** x}
 puts "Array mapping method:\n"
 puts "#{array_2}\n\n"
 
+# Solution 3
 array_3 = [0, 1, 2, 3, 4, 5, 6, 7].map {|number| 12 ** number}
 
 puts "Array mapping one-line method:\n"
--- step2.rb
+++ step2.rb
@@ -1,7 +1,9 @@
 # Expected result ["htxis", "htfif", "htruof", "driht", "dnoces", "tsrif"]
 
+# Starting array
 words = %w(first seconds third fourth fifth sixth)
 
+#solution 1
 words.reverse!.map! {|x| x.reverse}
 
 puts "#{words}"
